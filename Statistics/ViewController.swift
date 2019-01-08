1ab50de252ccca2fb9e70862463d302995834bf0
wave <zhanghaibo@bxzz.net>
2019-01-08 16:21:05 +0800

    Initial Commit

---
+++ Statistics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Statistics
+//
+//  Created by wave on 2019/1/8.
+//  Copyright © 2019 wave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
